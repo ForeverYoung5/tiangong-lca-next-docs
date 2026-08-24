0da3885b3ff8a758c18092d29768ebbd3edb02e9
nanli <linanenv@gmail.com>
2026-08-24 12:03:58 +0800

    feat: derive category directories from page tree (#148)

--- components/mdx.tsx
+++ components/mdx.tsx
@@ -1,5 +1,6 @@
 import defaultMdxComponents from 'fumadocs-ui/mdx';
 import type { MDXComponents } from 'mdx/types';
+import { CategoryDirectory } from '@/components/category-directory';
 import { DocsPortal } from '@/components/docs-portal';
 import { QuickStartGuide } from '@/components/quick-start-guide';
 import { VideoEmbed } from '@/components/video-embed';
@@ -7,6 +8,7 @@ import { VideoEmbed } from '@/components/video-embed';
 export function getMDXComponents(components?: MDXComponents) {
   return {
     ...defaultMdxComponents,
+    CategoryDirectory,
     DocsPortal,
     QuickStartGuide,
     VideoEmbed,
